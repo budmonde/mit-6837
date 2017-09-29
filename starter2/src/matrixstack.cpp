3a51c9d7c0eb9dbab96e47780f48b69e1e4c627c
Budmonde Duinkharjav <budmonde@gmail.com>
2017-09-28 23:50:51 -0400

    Complete Part 3.1

--- starter2/src/matrixstack.cpp
+++ starter2/src/matrixstack.cpp
@@ -2,25 +2,29 @@
 
 MatrixStack::MatrixStack() {
     // Initialize the matrix stack with the identity matrix.
+    m_matrices.push_back(Matrix4f::identity());
 }
 
 void MatrixStack::clear() {
     // Revert to just containing the identity matrix.
+    m_matrices.clear();
+    m_matrices.push_back(Matrix4f::identity());
 }
 
 Matrix4f MatrixStack::top() {
     // Return the top of the stack
     // return Matrix4f();
-
-    return Matrix4f::identity();
+    return *m_matrices.end();
 }
 
 void MatrixStack::push( const Matrix4f& m ) {
     // Push m onto the stack.
     // The new top should be "old * m", so that conceptually the new matrix
     // is applied first in right-to-left evaluation.
+    m_matrices.push_back(m);
 }
 
 void MatrixStack::pop() {
     // Remove the top element from the stack
+    m_matrices.pop_back();
 }
